abc0ab9124485f33357a36ccee8c90d1dca4fad8
Ihor <oleksandrenko1996@gmail.com>
2024-06-15 08:03:08 +0300

    create project fix scrool

--- style/js/javascript.js
+++ style/js/javascript.js
@@ -3,17 +3,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const mobileNav = document.getElementById('mobile-nav');
     const closeBtn = document.getElementById('close-btn');
 
+    // Function to disable scrolling
+    const disableScroll = () => {
+        document.body.style.overflow = 'hidden';
+    };
+
+    // Function to enable scrolling
+    const enableScroll = () => {
+        document.body.style.overflow = '';
+    };
+
     burgerMenu.addEventListener('click', () => {
         mobileNav.style.left = '0';
+        disableScroll(); // Disable scrolling when mobile nav is open
     });
 
     closeBtn.addEventListener('click', () => {
         mobileNav.style.left = '-100%';
+        enableScroll(); // Enable scrolling when mobile nav is closed
     });
 
     document.addEventListener('click', (event) => {
         if (!mobileNav.contains(event.target) && !burgerMenu.contains(event.target)) {
             mobileNav.style.left = '-100%';
+            enableScroll(); // Enable scrolling if clicked outside mobile nav
         }
     });
 });
